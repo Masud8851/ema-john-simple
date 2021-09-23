485dd8ca1e5cad025959305b2dfdd3b3cb993705
Masud8851 <masud1971.cse@gmail.com>
2021-09-23 17:58:07 +0600

    React

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,10 +2,23 @@ import './Cart.css'
 
 import React from 'react';
 
-const Cart = () => {
+const Cart = (props) => {
+
+    // console.log(props.cart)
+    const { cart } = props;
+    let total = 0;
+    for(const product of cart)
+    {
+        total = total + product.price;
+    }
+
+
     return (
         <div>
-            
+            <h4>Order Summary</h4>
+            <h5>Items Order: {props.cart.length} </h5>
+            <br />
+            <p>Total: {total} </p>
         </div>
     );
 };
