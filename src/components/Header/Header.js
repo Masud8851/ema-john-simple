70f7fd486c852364a52e34ba24a30bf57df7a2f5
Masud8851 <masud1971.cse@gmail.com>
2021-09-23 15:15:46 +0600

    React

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import logo from '../../images/logo.png'
 
 const Header = () => {
     return (
         <div>
             <h2>Hello</h2>
+            <img src={logo}/>
         </div>
     );
 };
