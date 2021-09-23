db1890ef29b2f8954ef82b182406a5682178a3ec
Masud8851 <masud1971.cse@gmail.com>
2021-09-23 16:26:50 +0600

    React

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -3,12 +3,13 @@ import './Product.css'
 
 const Product = (props) => {
     console.log(props)
-    const {name,img, price, stock } = props.product;
+    const {name,img, seller, price, stock } = props.product;
 
     return (
         <div>
             <img src={img} alt="" />
             <h2>{name}</h2>
+            <h4>{stock}</h4>
         </div>
     );
 };
