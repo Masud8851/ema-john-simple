77e0703e0176e8d0b67f8000ae7918d69aaa0b92
Masud8851 <masud1971.cse@gmail.com>
2021-09-24 11:36:39 +0600

    Add

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,15 +1,17 @@
 import './Cart.css'
-
 import React from 'react';
 
 const Cart = (props) => {
 
     // console.log(props.cart)
     const { cart } = props;
+
+    let totalQuantity = 0;
     let total = 0;
     for(const product of cart)
     {
-        total = total + product.price;
+        total = (total + product.price) * product.quantity ;
+        totalQuantity = totalQuantity + product.quantity;
     }
 
     const shipping = total > 0 ? 15 : 0;
@@ -19,7 +21,8 @@ const Cart = (props) => {
     return (
         <div>
             <h4>Order Summary</h4>
-            <h5>Items Order: {props.cart.length} </h5>
+            {/* <h5>Items Order: {props.cart.length} </h5> */}
+            <h5>Items Order: {totalQuantity} </h5>
             <br />
             <p>Total: {total.toFixed(2)} </p>
             <br />
